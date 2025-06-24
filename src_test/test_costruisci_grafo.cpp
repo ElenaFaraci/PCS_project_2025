0a91b4db5239db513a55dc355f32b13064d2c902
unknown <s307683@studenti.polito.it>
2025-06-24 11:53:53 +0200

    test grafo

--- src_test/test_costruisci_grafo.cpp
+++ src_test/test_costruisci_grafo.cpp
@@ -5,36 +5,41 @@
 #include <cmath>
 #include "Utils.hpp"  
 
-TEST(TestGrafo, CostruisciGrafoPesato) {
-    int n = 3;
-    Eigen::MatrixXi Cell1DsExtrema(2, 3);
-    Cell1DsExtrema << 0, 1, 2,
-                      1, 2, 0;
-    std::vector<Eigen::Vector3d> coords = {
-        {0.0, 0.0, 0.0},
-        {1.0, 0.0, 0.0},
-        {0.0, 1.0, 0.0}
-    };
+using namespace std;
 
-    Grafo grafo = costruisci_grafo_pesato(n, Cell1DsExtrema, coords);
+namespace PolygonalLibrary{
 
-    ASSERT_EQ(grafo.size(), 3);
-    
-    ASSERT_EQ(grafo[0].size(), 2);
-    EXPECT_EQ(grafo[0][0].first, 1);
-    EXPECT_DOUBLE_EQ(grafo[0][0].second, 1.0);
-    EXPECT_EQ(grafo[0][1].first, 2);
-    EXPECT_DOUBLE_EQ(grafo[0][1].second, 1.0);
+	TEST(TestGrafo, CostruisciGrafoPesato) {
+		int n = 3;
+		Eigen::MatrixXi Cell1DsExtrema(2, 3);
+		Cell1DsExtrema << 0, 1, 2,
+						  1, 2, 0;
+		std::vector<Eigen::Vector3d> coords = {
+			{0.0, 0.0, 0.0},
+			{1.0, 0.0, 0.0},
+			{0.0, 1.0, 0.0}
+		};
 
-    ASSERT_EQ(grafo[1].size(), 2);
-    EXPECT_EQ(grafo[1][0].first, 0);
-    EXPECT_DOUBLE_EQ(grafo[1][0].second, 1.0);
-    EXPECT_EQ(grafo[1][1].first, 2);
-    EXPECT_DOUBLE_EQ(grafo[1][1].second, std::sqrt(2.0));
+		Grafo grafo = costruisci_grafo_pesato(n, Cell1DsExtrema, coords);
 
-    ASSERT_EQ(grafo[2].size(), 2);
-    EXPECT_EQ(grafo[2][0].first, 1);
-    EXPECT_DOUBLE_EQ(grafo[2][0].second, std::sqrt(2.0));
-    EXPECT_EQ(grafo[2][1].first, 0);
-    EXPECT_DOUBLE_EQ(grafo[2][1].second, 1.0);
+		ASSERT_EQ(grafo.size(), 3);
+		
+		ASSERT_EQ(grafo[0].size(), 2);
+		EXPECT_EQ(grafo[0][0].first, 1);
+		EXPECT_DOUBLE_EQ(grafo[0][0].second, 1.0);
+		EXPECT_EQ(grafo[0][1].first, 2);
+		EXPECT_DOUBLE_EQ(grafo[0][1].second, 1.0);
+
+		ASSERT_EQ(grafo[1].size(), 2);
+		EXPECT_EQ(grafo[1][0].first, 0);
+		EXPECT_DOUBLE_EQ(grafo[1][0].second, 1.0);
+		EXPECT_EQ(grafo[1][1].first, 2);
+		EXPECT_DOUBLE_EQ(grafo[1][1].second, std::sqrt(2.0));
+
+		ASSERT_EQ(grafo[2].size(), 2);
+		EXPECT_EQ(grafo[2][0].first, 1);
+		EXPECT_DOUBLE_EQ(grafo[2][0].second, std::sqrt(2.0));
+		EXPECT_EQ(grafo[2][1].first, 0);
+		EXPECT_DOUBLE_EQ(grafo[2][1].second, 1.0);
+	}
 }
